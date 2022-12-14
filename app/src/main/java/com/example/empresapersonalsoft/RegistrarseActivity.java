e4b1b7c3cfc4b800cbaa782c734a62613cfa348f
vivilis85 <vivipaez381@gmail.com>
2022-12-14 17:50:14 -0500

    proyecto

--- app/src/main/java/com/example/empresapersonalsoft/RegistrarseActivity.java
+++ app/src/main/java/com/example/empresapersonalsoft/RegistrarseActivity.java
@@ -7,12 +7,26 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
-public class RegistrarseActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegistrarseActivity extends AppCompatActivity implements Response.Listener<JSONObject>,Response.ErrorListener {
 
     EditText jetusuario,jetcorreo,jetnombre,jetclave;
     CheckBox jcbactivo;
     String usuario,correo,nombre,clave,url;
+    RequestQueue rq;
+    JsonRequest jrq;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,7 @@ public class RegistrarseActivity extends AppCompatActivity {
         jetnombre = findViewById(R.id.etnombre);
         jetclave= findViewById(R.id.etclave);
         jcbactivo = findViewById(R.id.cbactivo);
+        rq = Volley.newRequestQueue(this);//conexion a internet
     }
 
     public void Consultar(View view){
@@ -33,7 +48,7 @@ public class RegistrarseActivity extends AppCompatActivity {
             Toast.makeText(this, "Usuario requerido", Toast.LENGTH_SHORT).show();
             jetusuario.requestFocus();
         }else{
-            url = "http://172.16.62.247:80/Empresa6/consulta.php?usr=" + usr;
+            url = "http://172.16.62.247:80/Empresa6/consulta.php?usr='" + usuario;
             jrq = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
             rq.add(jrq);
 
@@ -41,4 +56,32 @@ public class RegistrarseActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
+        jetusuario.requestFocus();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show();
+        JSONArray jsonArray = response.optJSONArray("datos");
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = jsonArray.getJSONObject(0);//posicion 0 del arreglo....
+            jetnombre.setText(jsonObject.optString("nombre"));
+            jetcorreo.setText(jsonObject.optString("correo"));
+            jetclave.setText(jsonObject.optString("clave"));
+            if (jsonObject.optString("activo").equals("si"))
+                jcbactivo.setChecked(true);
+            else
+                jcbactivo.setChecked(false);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
